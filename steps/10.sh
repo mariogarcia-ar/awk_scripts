f28589e9ddd6347ae5696ae25cac19995bde6a0c
Mario Garcia <mariogarcia.ar@gmail.com>
2024-03-31 21:47:10 -0300

    generate fake data

--- steps/10.sh
+++ steps/10.sh
@@ -14,46 +14,31 @@ if [ $run_step $comparison_operator 10 ]; then
     script=$SCRIPTS_DIR"/python/fake_data_generator.py"
     python "$script" 1000   -o "$output" -n 20
 
-    my_printf "Extrat money from utf-8"    
-    script=$SCRIPTS_DIR"/awk/research/extract_money.awk"
-    awk -f "$script" "$output"     
 
-    my_printf "Extrat extract_date_time from utf-8"    
-    script=$SCRIPTS_DIR"/awk/research/extract_date_time.awk"
-    awk -f "$script" "$output" 
+    # -------------------------------------------------------------------------
+    output=$TMP_DIR"/out_fake_data_generator_shuffle.txt"
 
-
+    my_printf "Generate Fake data with shuffle option"    
+    script=$SCRIPTS_DIR"/python/fake_data_generator.py"
+    python "$script" 1000   -o "$output" -n 20 -s
+ 
 
     # ========================================================================
     # es_ES
     # -------------------------------------------------------------------------
     output=$TMP_DIR"/out_fake_data_generator_es_ES.txt"
 
-    my_printf "Generate Fake data"    
+    my_printf "Generate Fake data with locale equal to es_ES"    
     script=$SCRIPTS_DIR"/python/fake_data_generator.py"
     python "$script" 1000   -o "$output" -n 20 -l "es_ES"
 
-    my_printf "Extrat money from utf-8"    
-    script=$SCRIPTS_DIR"/awk/research/extract_money.awk"
-    awk -f "$script" "$output"     
 
-    my_printf "Extrat extract_date_time from utf-8"    
-    script=$SCRIPTS_DIR"/awk/research/extract_date_time.awk"
-    awk -f "$script" "$output" 
+    # -------------------------------------------------------------------------
+    output=$TMP_DIR"/out_fake_data_generator_es_ES_shuffle.txt"
 
-
-
-    exit 1
-   # -------------------------------------------------------------------------
-    output=$TMP_DIR"/out_fake_data_generator_suffle.txt"
-
-    my_printf "Generate Fake data with suffle option"    
+    my_printf "Generate Fake data with shuffle option and locale equal to es_ES"    
     script=$SCRIPTS_DIR"/python/fake_data_generator.py"
-    python "$script" 1000   -o "$output" -s -n 20
-
-
-    my_printf "Extrat money from utf-8"    
-    script=$SCRIPTS_DIR"/awk/research/extract_money.awk"
-    awk -f "$script" "$output"     
+    python "$script" 1000   -o "$output" -n 20 -s -l "es_ES"
+ 
 
 fi
